fad9e2108a25a0f31db14902a45c09212f8b924f
Andrey Rezende <dreyrezende@hotmail.com>
2022-02-02 22:59:12 -0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/randomix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.randomix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'app/modules/home/screen.dart';
+
+void main() {
+  runApp(const Randomix());
+}
+
+class Randomix extends StatelessWidget {
+  const Randomix({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Randomix',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
