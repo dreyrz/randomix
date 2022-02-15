d6eb6ff4ac68e208a4399d3a60838de1daa2c1e9
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2022-02-15 20:24:10 -0300

    unit tests for new methods added

--- test/app/core/services/storage_test.dart
+++ test/app/core/services/storage_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:randomix/app/core/services/storage.dart';
 
-import '../../mocks/utils_mock.dart';
+import '../../mocks/utils.dart';
 
 main() {
   late final IStorageService service;
@@ -15,16 +15,48 @@ main() {
     service = Get.find<IStorageService>();
   });
 
-  test('Expect to be a bool value when a bool value is saved', () async {
-    await service.setBool('key', true);
-    final value = service.readBool('key');
-    expect(value, true);
+  group('Storage String methods', () {
+    test('Expect to be maintain the value when it has been saved', () async {
+      await service.setString('key', anyString);
+      final value = service.readString('key');
+      expect(value, anyString);
+    });
+
+    test('Expect to change the value', () async {
+      await service.setString('key', anyString);
+      await service.setString('key', 'random String');
+      final value = service.readString('key');
+      expect(value, 'random String');
+    });
+
+    test('Expect to have a String value removed', () async {
+      service.setString('key', anyString);
+      await service.remove('key');
+      final value = service.readString('key');
+      expect(value, null);
+    });
   });
 
-  test('Expect to be a String value when a String value is saved', () async {
-    await service.setString('key', anyString);
-    final value = service.readString('key');
-    expect(value, anyString);
+  group('Storage bool methods', () {
+    test('Expect to be maintain the value when it has been saved', () async {
+      await service.setBool('key', true);
+      final value = service.readBool('key');
+      expect(value, true);
+    });
+
+    test('Expect to change the value', () async {
+      await service.setBool('key', true);
+      await service.setBool('key', false);
+      final value = service.readBool('key');
+      expect(value, false);
+    });
+
+    test('Expect to have a Bool value removed', () async {
+      service.setBool('key', true);
+      await service.remove('key');
+      final value = service.readBool('key');
+      expect(value, null);
+    });
   });
 
   //TODO: Test remove, test null error cases
