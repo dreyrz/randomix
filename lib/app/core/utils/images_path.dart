a00771bac4bb881ae13d49b654c022066a66c3b5
Andrey Rezende <dreyrezende@hotmail.com>
2022-03-10 12:11:45 -0300

    image svgs

--- lib/app/core/utils/images_path.dart
+++ lib/app/core/utils/images_path.dart
@@ -1,4 +1,6 @@
 abstract class SvgImagesPath {
   static const _basePath = 'assets/svgs';
-  static const aboutImage = '$_basePath/about.svg';
+  static const about = '$_basePath/about.svg';
+  static const empty = '$_basePath/empty.svg';
+  static const shuffle = '$_basePath/shuffle.svg';
 }
