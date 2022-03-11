1841650cc4e5b4a6e1639944d4a1cc5312f87140
Andrey Rezende <dreyrezende@hotmail.com>
2022-03-11 19:05:26 -0300

    GenresDropdown widget design

--- lib/app/modules/home/widgets/genres_dropdown.dart
+++ lib/app/modules/home/widgets/genres_dropdown.dart
@@ -21,25 +21,51 @@ class _GenresSearchState extends State<GenresDropDown> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
-      value: value,
-      items: widget.genres
-          .map(
-            (genre) => DropdownMenuItem(
-              value: genre,
-              child: Text(
-                genre.replaceRange(0, 1, genre.substring(0, 1).toUpperCase()),
-                style: Theme.of(context).textTheme.headline3,
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      margin: const EdgeInsets.symmetric(horizontal: 32),
+      decoration: BoxDecoration(
+          color: Theme.of(context).secondaryHeaderColor,
+          borderRadius: BorderRadius.circular(6)),
+      child: DropdownButton(
+        isExpanded: true,
+        value: value,
+        dropdownColor: Theme.of(context).secondaryHeaderColor,
+        icon: Icon(
+          Icons.arrow_drop_down,
+          color: Theme.of(context).backgroundColor,
+        ),
+        underline: Container(
+          height: 1.0,
+          decoration: const BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.transparent,
               ),
             ),
-          )
-          .toList(),
-      onChanged: (String? newValue) {
-        setState(() {
-          value = newValue!;
-          widget.onGenreSelected(newValue);
-        });
-      },
+          ),
+        ),
+        items: widget.genres
+            .map(
+              (genre) => DropdownMenuItem(
+                value: genre,
+                child: Text(
+                  genre.replaceRange(0, 1, genre.substring(0, 1).toUpperCase()),
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline3!
+                      .copyWith(color: Theme.of(context).backgroundColor),
+                ),
+              ),
+            )
+            .toList(),
+        onChanged: (String? newValue) {
+          setState(() {
+            value = newValue!;
+            widget.onGenreSelected(newValue);
+          });
+        },
+      ),
     );
   }
 }
