09b3038b5bee10776e3af6bb559af2ad8b958359
dreyrz <dreyrezende@hotmail.com>
2022-12-28 11:22:58 -0300

    CustomImage loadingBuilder

--- lib/app/core/widgets/custom_image.dart
+++ lib/app/core/widgets/custom_image.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:randomix/app/core/widgets/loading.dart';
 
 class CustomImage extends Image {
-  CustomImage(String src, {Key? key, double? height, double? width})
-      : super.network(
+  CustomImage(
+    String src, {
+    double? height,
+    double? width,
+    Key? key,
+  }) : super.network(
           src,
           errorBuilder: (a, b, c) => const Icon(Icons.error),
+          loadingBuilder: (context, image, loading) {
+            if (loading != null) {
+              return SizedBox(
+                height: height ?? 80,
+                width: width ?? 80,
+                child: Center(
+                  child: Loading(
+                    color: Theme.of(context).primaryColor,
+                    size: 30,
+                    strokeWidth: 2,
+                  ),
+                ),
+              );
+            } else {
+              return image;
+            }
+          },
           height: height,
           width: width,
           key: key,
