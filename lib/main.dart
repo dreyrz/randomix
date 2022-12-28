969a11823f477a16eab80f2dfd84b29ab01f082b
dreyrz <dreyrezende@hotmail.com>
2022-12-28 16:40:04 -0300

    removing unecessary bindings in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'app/core/bindings/bindings.dart';
-import 'app/core/routes/pages.dart';
-import 'app/core/routes/routes.dart';
+import 'app/core/bindings/_bindings.dart';
+import 'app/core/routes/_routes.dart';
 import 'app/core/services/_services.dart';
-import 'app/core/themes/colors.dart';
-import 'app/core/themes/text_styles.dart';
-import 'app/core/themes/themes.dart';
-import 'app/core/utils/dispatcher_callback.dart';
+import 'app/core/themes/_themes.dart';
+import 'app/core/utils/_utils.dart';
 
-// top level function required
+// top level function required for background service
 @pragma('vm:entry-point')
 Future<void> _taskDispatcher() async {
   _backgroundTaskService.dispatcher(dispatcherCallback);
 }
 
 final IBackgroundTaskService _backgroundTaskService = BackgroundTaskService();
-late INotificationService _notificationService;
-late ITrackListService _trackListService;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  _notificationService = NotificationService(RandomixColors());
-  _trackListService = TrackListService();
-  Get.put(_notificationService);
-  Get.put<ITrackListService>(_trackListService);
 
   await BackgroundTaskService().init(_taskDispatcher);
 
