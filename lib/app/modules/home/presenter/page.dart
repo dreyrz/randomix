5cf90f485fd7aaa47451e2ee95f5af514a1b9285
Andrey Rezende <dreyrezende@hotmail.com>
2022-12-29 20:39:00 -0300

    fix pop bug in home

--- lib/app/modules/home/presenter/page.dart
+++ lib/app/modules/home/presenter/page.dart
@@ -13,59 +13,57 @@ class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => Scaffold(
-        appBar: HomeAppBar(
-          isNotificationsEnabled: controller.isNotificationsEnabled.value,
-          onNotificationIconPressed: () async =>
-              controller.enableNotifications(context),
-        ),
-        body: Navigator(
-          initialRoute: Routes.home,
-          key: Get.nestedKey(0),
-          onGenerateRoute: (settings) {
-            return controller.tabNavigator.onRouteChanged(
-              settings: settings,
-              firstTabRoute: Routes.home,
-              basePage: Container(
-                color: Theme.of(context).backgroundColor,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Spacer(flex: 10),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          'Clique para obter uma música!',
-                          style: Theme.of(context).textTheme.headline2,
-                        ),
-                      ),
-                      const Spacer(flex: 35),
-                      SizedBox(
-                        height: 300,
-                        child: CircularButton(
-                          svgPath: SvgPath.shuffle,
-                          onPressed: () async =>
-                              await controller.getRandomTrack(context),
-                        ),
-                      ),
-                      const Spacer(flex: 5),
-                      GenresDropDown(
-                        controller.genresList,
-                        controller.setGenreSelected,
-                      ),
-                      const Spacer(flex: 55),
-                    ],
+    return Navigator(
+      // initialRoute: Routes.home,
+      key: Get.nestedKey(0),
+      onGenerateRoute: (settings) {
+        return controller.tabNavigator.onRouteChanged(
+            settings: settings,
+            firstTabRoute: Routes.home,
+            basePage: Obx(() => Scaffold(
+                  appBar: HomeAppBar(
+                    isNotificationsEnabled:
+                        controller.isNotificationsEnabled.value,
+                    onNotificationIconPressed: () async =>
+                        controller.enableNotifications(context),
                   ),
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+                  body: Container(
+                    color: Theme.of(context).backgroundColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Spacer(flex: 10),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              'Clique para obter uma música!',
+                              style: Theme.of(context).textTheme.headline2,
+                            ),
+                          ),
+                          const Spacer(flex: 35),
+                          SizedBox(
+                            height: 300,
+                            child: CircularButton(
+                              svgPath: SvgPath.shuffle,
+                              onPressed: () async =>
+                                  await controller.getRandomTrack(context),
+                            ),
+                          ),
+                          const Spacer(flex: 5),
+                          GenresDropDown(
+                            controller.genresList,
+                            controller.setGenreSelected,
+                          ),
+                          const Spacer(flex: 55),
+                        ],
+                      ),
+                    ),
+                  ),
+                )));
+      },
     );
   }
 }
